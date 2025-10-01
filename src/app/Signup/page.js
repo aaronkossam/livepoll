602ca37464c620f580d203f96cbb7be289d53202
Aaronkossam <aaronkossam23@gmail.com>
2025-10-01 13:51:55 +0000

    feat: enhance signup page with background image and animations

--- src/app/Signup/page.js
+++ src/app/Signup/page.js
@@ -10,8 +10,12 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { motion } from "framer-motion";
+
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+
 import Link from "next/link";
 const Signup = () => {
   const [email, setEmail] = React.useState("");
@@ -42,60 +46,91 @@ const Signup = () => {
   };
 
   return (
-    <div>
-      <Card className="w-full xl:w-screen 2xl:w-screen xl:max-w-sm 2xl:max-w-3xl 2xl:max-h-lvh  max-w-sm">
-        <CardHeader>
-          <CardTitle>Sign up tp create a staff account</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
-          <CardAction>
-            <Link
-              href="/clientSignInForm"
-              className="underline -mt-3.5"
-              variant="link"
-            >
-              Login{" "}
-            </Link>
-          </CardAction>
-        </CardHeader>
-        <CardContent>
-          <form onSubmit={handleSubmit}>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  onChange={(e) => setEmail(e.target.value)}
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    Forgot your password?
-                  </a>
+    <div
+      style={{
+        backgroundImage: 'url("/Artboard 1.jpg")',
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+      className="bg-cover bg-center h-screen "
+    >
+      <div className="pt-16 xl:pt-0 xl:justify-end 2xl:pt-64 xl:pr-14 xl:grid ">
+        {/* {container} */}
+        <div className="pt-16 ">
+          {/* Title */}
+          <motion.div
+            className="text-center"
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, ease: "easeOut" }}
+          >
+            <p className="text-6xl font-black">HDEX INC</p>
+            <span className="italic  ">Create your Staff Account </span>
+          </motion.div>
+        </div>
+
+        {/* SigninForm */}
+        <motion.div
+          className="pr-1 pl-1"
+          initial={{ opacity: 0, scale: 0.95 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ delay: 1, duration: 0.8 }}
+        ></motion.div>
+
+        <Card className="w-full xl:w-screen  2xl:w-screen xl:max-w-sm 2xl:max-w-3xl 2xl:max-h-lvh  max-w-sm">
+          <CardHeader>
+            <CardTitle>Sign up tp create a staff account</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account
+            </CardDescription>
+            <CardAction>
+              <Link
+                href="/clientSignInForm"
+                className="underline -mt-3.5"
+                variant="link"
+              >
+                Login{" "}
+              </Link>
+            </CardAction>
+          </CardHeader>
+          <CardContent>
+            <form onSubmit={handleSubmit}>
+              <div className="flex flex-col gap-6">
+                <div className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    onChange={(e) => setEmail(e.target.value)}
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    required
+                  />
                 </div>
-                <Input
-                  id="password"
-                  onChange={(e) => setPassword(e.target.value)}
-                  type="password"
-                  required
-                />
+                <div className="grid gap-2">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                    <a
+                      href="#"
+                      className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                    >
+                      Forgot your password?
+                    </a>
+                  </div>
+                  <Input
+                    id="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    type="password"
+                    required
+                  />
+                </div>
               </div>
-            </div>
-            <Button type="submit" className=" mt-3.5 w-full">
-              Sign up
-            </Button>
-          </form>
-        </CardContent>
-      </Card>
+              <Button type="submit" className=" mt-3.5 w-full">
+                Sign up
+              </Button>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 };
